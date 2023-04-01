62665a5cfc01c7bdee7102a16b64e805e04a4425
Andre Gomes <andre@appdev.com.br>
2023-04-01 16:27:40 -0300

    First Commit

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,60 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/library",
+    "sap/ui/model/json/JSONModel"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, library, JSONModel) {
+        "use strict";
+        let urlObject = library.URLHelper;
+
+        return Controller.extend("consultaprodutos.controller.Main", {
+            onInit: function () {
+                let produto = {};
+                let productModel = new JSONModel(produto);
+                let view = this.getView();
+                view.setModel(productModel, "ModeloProduto");
+                // this.getView().setModel(productModel, "produto");
+            },
+            onClickImage: function (oEvent) {
+                urlObject.redirect(oEvent.getSource().getSrc(), true);
+            },
+            onPressBuscar: function (oEvent) {
+                let input;
+                input = this.byId("inpBusca");
+                let valor =  input.getValue();
+
+                let parameters = {
+                    url : "https://world.openfoodfacts.org/api/v2/product/" + valor,
+                    method : "GET",
+                    async : true,
+                    crossDomain : true
+                };
+
+                $.ajax(parameters).done(function (data) {
+                    let oProdutoModel = this.getView().getModel("ModeloProduto");
+                    oProdutoModel.setData({});
+                    oProdutoModel.refresh();
+                    oProdutoModel.setData(data);
+                    oProdutoModel.refresh();
+
+                }.bind(this)).fail(function (err) {
+                    console.log(err);
+                }.bind(this));
+                // let material = "Agua Mineral Natural"
+                // let peso = 500;
+                // let uom = "ml";
+                // let qtdsodio = 15.66;
+                // let conteudoliquido = true;
+                // let composicao = ["bicarbonato", "magnesio", "Minerais", "sulfato", "brometo"];
+                // let produto = {
+                //     descricao: "chá verde",
+                //     marca: "quaker",
+                //     peso: 130,
+                //     uom: "g"
+                // }
+            }
+        });
+    });
